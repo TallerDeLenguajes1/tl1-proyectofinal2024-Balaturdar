1f44285b69f84e5b1c5fbb4fd3156a8f22ebb341
VillafañeNahuel <151423052+Balaturdar@users.noreply.github.com.>
2024-06-24 22:29:38 -0300

    correccion de se cambiaron las armas a una sola lista y se agrego un control de fuerza requerida para armas y escudo

--- FabricaPersonajes.cs
+++ FabricaPersonajes.cs
@@ -3,9 +3,7 @@ using System.Text.Json;
 public class FabricaPersonajes
 {
     List<Armadura>? armaduras;
-    List<Arma>? armasComunes;
-    List<Arma>? armasExoticas;
-    List<Arma>? armasImprovisadas;
+    List<Arma>? armas;
     List<Arma>? escudos;
     List<Categoria>? categorias;
     string[] nombres;
@@ -17,15 +15,9 @@ public class FabricaPersonajes
         string jsonDocument = miGestor.AbrirArchivoTexto("armaduras.json");
         this.armaduras = JsonSerializer.Deserialize<List<Armadura>>(jsonDocument);
                 
-        jsonDocument = miGestor.AbrirArchivoTexto("armasComunes.json");
-        this.armasComunes = JsonSerializer.Deserialize<List<Arma>>(jsonDocument);
+        jsonDocument = miGestor.AbrirArchivoTexto("armas.json");
+        this.armas = JsonSerializer.Deserialize<List<Arma>>(jsonDocument);
 
-        jsonDocument = miGestor.AbrirArchivoTexto("armasExoticas.json");
-        this.armasExoticas = JsonSerializer.Deserialize<List<Arma>>(jsonDocument);
-
-        jsonDocument = miGestor.AbrirArchivoTexto("armasImprovisadas.json");
-        this.armasImprovisadas = JsonSerializer.Deserialize<List<Arma>>(jsonDocument);
-        
         jsonDocument = miGestor.AbrirArchivoTexto("categorias.json");
         this.categorias = JsonSerializer.Deserialize<List<Categoria>>(jsonDocument);
 
@@ -39,7 +31,7 @@ public class FabricaPersonajes
     public Personaje CrearPersonaje(int i)
     {
         Random rnd = new Random();
-        int PD= (300+i*100)/2; //200
+        int PD= (300+i*100)/2; 
         string nombre = nombres[rnd.Next(0,nombres.Count()+1)];
         string apodo = apodos[rnd.Next(0,apodos.Count())+1];;
         int edad = rnd.Next(20, 301);
@@ -53,28 +45,28 @@ public class FabricaPersonajes
         int hDefBase = PD - hAtaBase;
 
         Categoria cat = categorias.ElementAt(rnd.Next(0, categorias.Count+1));
-        Arma? armaPj = null;
-        switch (rnd.Next(1, 4)) // cambiar a un solo Json
-        {
-            case 1:
-                do
-                {
-                    armaPj = armasComunes.ElementAt(rnd.Next(0, armasComunes.Count+1));
-                } while (armaPj.FueR <= fue);
-                break;
-            case 2:
-                armaPj = armasExoticas.ElementAt(rnd.Next(0, armasExoticas.Count+1));
-                break;
-            case 3:
-                armaPj = armasImprovisadas.ElementAt(rnd.Next(0, armasImprovisadas.Count+1));
-                break;
+
+        Arma armaPj;
+        if(fue < 3){
+            armaPj = armas.ElementAt(41);
+        }else{
+            do
+            {
+                armaPj = armas.ElementAt(rnd.Next(0, armas.Count+1));
+            } while (armaPj.FueR < fue);
         }
+        
         Armadura armadura = armaduras.ElementAt(rnd.Next(0, armaduras.Count+1));
+
         Arma escudo;
-        do
-        {
-            escudo = escudos.ElementAt(rnd.Next(0,escudos.Count+1));
-        } while (escudo.FueR <= fue);
+        if(fue < 5){
+            escudo = escudos.ElementAt(3);
+        }else{
+            do
+            {
+                escudo = escudos.ElementAt(rnd.Next(0,escudos.Count+1));
+            } while (escudo.FueR <= fue);
+        }
 
         return new Personaje(nombre, apodo,fecNaci,edad,fue,des,agi,con,nivel,hAtaBase,hDefBase,cat,armaPj,armadura,escudo);
     }
