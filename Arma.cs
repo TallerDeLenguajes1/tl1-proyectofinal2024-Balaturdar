b398b725a28993bb86c56c1908deb2f64b4a31d7
VillafañeNahuel <151423052+Balaturdar@users.noreply.github.com.>
2024-06-24 22:27:36 -0300

    correccion de nombres en la clase Armas

--- Arma.cs
+++ Arma.cs
@@ -1,12 +1,31 @@
+using System.Text.Json.Serialization;
+
 public class Arma{
     private string nombre;
     private int danio;
     private int turno;
     private int fueR;
     private string crt1;
-    private string crt2;
+    private string? crt2;
     private string tArma;
     private string especial;
+
+    [JsonPropertyName("Nombre")]
+    public string Nombre { get; set; }
+    [JsonPropertyName("Danio")]
+    public int Danio { get; set; }
+    [JsonPropertyName("Turno")]
+    public int Turno { get; set; }
+    [JsonPropertyName("FueR")]
+    public int FueR { get; set; }
+    [JsonPropertyName("TAtaque")]
+    public string TAtaque { get; set; }
+    [JsonPropertyName("TAtaque 2")]
+    public string TAtaque2 { get; set; }
+    [JsonPropertyName("Tarma")]
+    public string Tarma { get; set; }
+    [JsonPropertyName("Especial")]
+    public string Especial { get; set; }
 
     public Arma(string nombre, int danio, int turno, int fueR, string crt1, string crt2, string tArma, string especial)
     {
@@ -20,12 +39,5 @@ public class Arma{
         this.especial = especial;
     }
 
-    public string Nombre { get => nombre; set => nombre = value; }
-    public int Danio { get => danio; set => danio = value; }
-    public int Turno { get => turno; set => turno = value; }
-    public int FueR { get => fueR; set => fueR = value; }
-    public string Crt1 { get => crt1; set => crt1 = value; }
-    public string Crt2 { get => crt2; set => crt2 = value; }
-    public string TArma { get => tArma; set => tArma = value; }
-    public string Especial { get => especial; set => especial = value; }
 }
+
